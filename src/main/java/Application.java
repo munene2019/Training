d646f3ad7964e605a5867a77c38c3ce8a0ec5330
Rogers <munenerogers@gmail.com>
2022-10-20 11:41:22 +0300

    First project

---
+++ src/main/java/Application.java
@@ -0,0 +1,28 @@
+public class Application {
+
+    public static void main (String arg[]){
+        String name="Mwendi";
+        int nationalId=270973783;
+        boolean youth=true;
+        double accountBalance=300.939393788888888888888888888888888888888888888888887667778899999;
+        float accountBal=30000000;
+        System.out.println("Student Name " + name);
+
+
+
+    }
+    //What is a:
+    //Class-Application
+    //Method/function-main
+    //Variable
+    //type-String/Integer/boolean/double/float/
+    //Declaration
+    //concatenation
+    //Compile
+
+
+    //return type
+
+
+
+}
